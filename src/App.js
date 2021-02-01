be04d712e958ffafecdf30c229ed4e45d6ffea71
Hyungjun Rho <zuntopia@gmail.com>
2021-02-01 23:53:28 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   const name = "foo";
   return (
     <div>
-      <Wrapper>
-        <Hello name="react" color="red" isSpecial={true}/>
-        <Hello color="pink" />
-      </Wrapper>
-      <Hello name="react" color="red"/>
-      <div className="App-header"><Hello /></div>
-      <Hello />
-      <Hello />
-      <div>{name}</div>
+      <Counter />
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Counter () {
+  const onIncrease = () => {
+    console.log('+1')
+  }
+  const onDecrease = () => {
+    console.log('-1')
+  }
+  return (
+    <div>
+      <h1>0</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  )
+}
+
+export default Counter;
