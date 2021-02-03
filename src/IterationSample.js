7fa9da7952536378c42e97d03a87c4926fb94046
Hyungjun Rho <zuntopia@gmail.com>
2021-02-03 23:01:42 +0900

    Iteration
    concat to add item on map
    filter to remove item from map

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
 
-  let numbers = [1, 2, 3, 4, 5];
-  const processed = numbers.map(name => <li key={name}>{name*name}</li>);
+  // let numbers = [1, 2, 3, 4, 5];
+  const [names, setNames] = useState([
+    {'id': 1, text: "hello"},
+    {'id': 2, text: "world"},
+    {'id': 3, text: "foo"},
+    {'id': 4, text: "bar"},
+  ]);
+
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+  const processed = names.map(name => (<li key={name.id} onDoubleClick={()=>onRemove(name.id)}>{name.text}</li>));
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+      }
+    );
+    setNextId(nextId+1);
+    setNames(nextNames);
+    setInputText('');
+  }
+  const onRemove = id => {
+    const nextNames = names.filter(name => name.id !== id);
+    setNames(nextNames);
+  }
   return (
+    <>
     <ul>{processed}</ul>
+    <input value={inputText} onChange={onChange} />
+    <button onClick={onClick}>Add </button>
+
+    </>
   )
 }
 
